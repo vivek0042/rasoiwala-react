b8fc1bd90694734ed39de97dcdbecaf8ae0c98bc
vivek jain <100334280+vivek0042@users.noreply.github.com>
2023-04-02 14:00:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Navbar from './Navbar'
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 // import "../node_modules/bootstrap/dist/js/bootstrap.bundle"
 import("bootstrap");
-import {BrowserRouter} from 'react-router-dom'
+// import {BrowserRouter} from 'react-router-dom'
 
 const App = () => {
   return (
