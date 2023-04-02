0b938f61ce26bf8310601aac479623228abc7ac8
vivek0042 <vivekjain.0042@gmail.com>
2023-04-02 11:10:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Galleryshow from './galleryshow'
+import Navbar from './Navbar'
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle"
+import {BrowserRouter} from 'react-router-dom'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <BrowserRouter>
+    <div>
+    {/* <Navbar/> */}
+    <Galleryshow/>
     </div>
-  );
+      
+    // </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
+
