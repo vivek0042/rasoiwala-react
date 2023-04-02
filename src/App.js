e1186e1f0656348420a41eb5071bd5b575242994
vivek jain <100334280+vivek0042@users.noreply.github.com>
2023-04-02 13:44:41 +0530

    commit 2 boot js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Galleryshow from './galleryshow'
 import Navbar from './Navbar'
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 // import "../node_modules/bootstrap/dist/js/bootstrap.bundle"
+import("bootstrap");
 import {BrowserRouter} from 'react-router-dom'
 
 const App = () => {
